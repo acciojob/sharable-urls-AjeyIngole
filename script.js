57c45118930ad4ffda06cbe6d85f4ef33f6ed777
AjeyIngole <108457642+AjeyIngole@users.noreply.github.com>
2023-09-21 14:51:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // your code here
+document.getElementById('urlForm').addEventListener('submit', function (event) {
+            event.preventDefault(); // Prevent the form from submitting normally
+            
+            // Get values from the form inputs
+            const name = document.getElementById('name').value;
+            const year = document.getElementById('year').value;
+            
+            // Build the query string
+            const queryString = `?name=${encodeURIComponent(name)}&year=${encodeURIComponent(year)}`;
+            
+            // Update the URL in the h3 element
+            const urlElement = document.getElementById('url');
+            urlElement.textContent = `https://localhost:8080/${queryString}`;
+        });
